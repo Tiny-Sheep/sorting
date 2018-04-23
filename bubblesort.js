d32b1dab9590bb2115989f07be10cccf0b2b82fc
chawes <hawes.conor@gmail.com>
2018-04-23 11:46:43 -0500

    implemented first draft

--- bubblesort.js
+++ bubblesort.js
@@ -1,18 +1,31 @@
 function bubbleSort(array) {
+  // Compare adjacent neighbors and perform swap if necessary
+  // Repeat until stored
 
-  /* your code here */
+  let swaps = -1;
+  let arrLength = array.length;
+  while (swaps !== 0) {
+    swaps = 0;
+    for (let i=0; i < arrLength; i++) {
+      if (arrLength > 1 && compareFn(array[i], array[i+1])) {
+        swapFn(array, i);
+        swaps++;
+      }
+    }
+    arrLength--;
+  }
 
-  return array
-
+  return array;
 }
 
 
 
-function swapFn (index1,index2){
-
-  
+function swapFn (array, currentIndex){
+  let temp = array[currentIndex];
+  array[currentIndex] = array[currentIndex + 1];
+  array[currentIndex + 1] = temp;
 }
 
-function compareFn(num1,num2){
-
+function compareFn(num1, num2){
+  return num2 < num1;
 }
