b25304fbef5fad09ae5e9a8b9fe4abe453f97fff
Tiny-Sheep <lsadoun@mail.bradley.edu>
2018-04-23 12:21:15 -0500

    bubble sort completed

--- bubblesort.js
+++ bubblesort.js
@@ -1,23 +1,54 @@
+// function bubbleSort(array) {
+//   // Compare adjacent neighbors and perform swap if necessary
+//   // Repeat until stored
+//
+//   let swaps = -1;
+//   let arrLength = array.length;
+//   while (swaps !== 0) {
+//     swaps = 0;
+//     for (let i=0; i < arrLength-1; i++) {
+//       if (arrLength > 1 && compareFn(array[i], array[i+1])) {
+//         swapFn(array, i);
+//         swaps++;
+//       }
+//     }
+//     arrLength--;
+//   }
+//
+//   return array;
+// }
+
+
+
+
 function bubbleSort(array) {
   // Compare adjacent neighbors and perform swap if necessary
   // Repeat until stored
 
-  let swaps = -1;
-  let arrLength = array.length;
-  while (swaps !== 0) {
-    swaps = 0;
-    for (let i=0; i < arrLength; i++) {
-      if (arrLength > 1 && compareFn(array[i], array[i+1])) {
+  // base case
+if(array.length === 1 || array.length === 0){
+  return array
+}
+//recursive case
+let swaps = 0
+for (let i=0; i < array.length-1; i++) {
+
+      if (compareFn(array[i], array[i+1])) {
         swapFn(array, i);
-        swaps++;
+        swaps++
+
       }
     }
-    arrLength--;
-  }
-
-  return array;
+if(!swaps){
+  return array
 }
 
+return bubbleSort(array.slice(0,-1)).concat(array[array.length-1])
+
+
+
+
+}
 
 
 function swapFn (array, currentIndex){
